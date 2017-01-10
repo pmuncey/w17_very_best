cb668386418d9dcf3164e88a668db669629cdc18
Raghu Betina <raghu@firstdraft.com>
2017-01-10 20:10:30 +0000

    Direct association between foods and very_bests

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,10 @@
 class Food < ApplicationRecord
   # Direct associations
 
+  has_many   :foodvenues,
+             :class_name => "VeryBest",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/very_best.rb
+++ app/models/very_best.rb
@@ -1,6 +1,8 @@
 class VeryBest < ApplicationRecord
   # Direct associations
 
+  belongs_to :food
+
   # Indirect associations
 
   # Validations
