5eed8383519aefc6e9e5d3e1482f5c537d5a734e
Raghu Betina <raghu@firstdraft.com>
2017-01-10 20:10:30 +0000

    Direct association between cuisines and foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,8 @@
 class Food < ApplicationRecord
   # Direct associations
 
+  belongs_to :cuisine
+
   has_many   :foodvenues,
              :class_name => "VeryBest",
              :dependent => :destroy
