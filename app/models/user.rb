98e0a8582d59ee78abc33dd46aba10c9ea465db2
Raghu Betina <raghu@firstdraft.com>
2017-01-10 20:10:30 +0000

    Direct association between users and very_bests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :very_bests,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/very_best.rb
+++ app/models/very_best.rb
@@ -1,6 +1,8 @@
 class VeryBest < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   belongs_to :venue
 
   belongs_to :food
